c1ac513ff475cf5a3eb50e769f46943ed4e32148
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-11-30 18:23:21 +0200

    better color values

--- BMI Calculator/Model/CalculatorBrain.swift
+++ BMI Calculator/Model/CalculatorBrain.swift
@@ -27,11 +27,11 @@ struct CalculatorBrain {
     mutating func calculate(height: Float, weight: Float){
         let bmiValue = weight / pow(height,2)
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.init(red: 0, green: 253, blue: 255, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
         } else if bmiValue < 24.9 {
-            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color:UIColor.init(red: 0, green: 249, blue: 0, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
         } else {
-            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.init(red: 255, green: 38, blue: 0, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
         }
     }
     
